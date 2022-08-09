92c600d95841c39e015796bcf1cb16e9d7aea758
weason <9384835+tangweason@user.noreply.gitee.com>
2022-08-09 23:12:49 +0800

    新增关闭浏览器和退出程序逻辑

--- main/main.go
+++ main/main.go
@@ -12,23 +12,37 @@ import (
 var port = "27149"
 
 func main() {
+	chChromeDie := make(chan struct{})
+	chBackendDie := make(chan struct{})
 	go server.Run(port)
-	cmd := startBrowser(port)
+	go startBrowser(port, chChromeDie, chBackendDie)
 	chSignal := listenToInterrupt()
 	//等待中断信号
-	select {
-	case <-chSignal:
-		cmd.Process.Kill()
+	for {
+		select {
+		case <-chSignal:
+			chBackendDie <- struct{}{}
+		case <-chChromeDie:
+			os.Exit(0)
+		}
 	}
 }
 
 //启动 Chrome
 // 先写死路径，后面再照着 lorca 改
-func startBrowser(port string) *exec.Cmd {
+func startBrowser(port string, chChromeDie chan struct{},
+	chBackendDie chan struct{}) {
 	chromePath := "C:/Program Files/Google/Chrome/Application/chrome.exe"
 	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:"+port+"/static/index.html")
 	cmd.Start()
-	return cmd
+	go func() {
+		<-chBackendDie
+		cmd.Process.Kill()
+	}()
+	go func() {
+		cmd.Wait()
+		chChromeDie <- struct{}{}
+	}()
 }
 
 //监听中断信号
